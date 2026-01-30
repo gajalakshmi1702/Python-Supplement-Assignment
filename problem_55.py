181fd6d0f6e6d70f56d5742339fbc515ecb488f2
gajalakshmi1702 <gajalakshmi1702@gmail.com>
2026-01-30 14:57:51 +0530

    problem 55 solved

--- problem_55.py
+++ problem_55.py
@@ -1,14 +1,10 @@
 # Problem 55: Count frequency of each element
 # Find and fix the error
 
+from collections import Counter
+
 def count_frequency(lst):
-    freq = {}
-    for item in lst:
-        if item in freq:
-            freq[item] += 1
-        else:
-            freq[item] = 1
-    return freq
+    return dict(Counter(lst))
 
 numbers = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
 print(f"Frequency: {count_frequency(numbers)}")
