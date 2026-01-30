25333f2cb9e701b2b34c4c911ae3dcf154d45d8b
gajalakshmi1702 <gajalakshmi1702@gmail.com>
2026-01-30 14:35:48 +0530

    problem 44 solved

--- problem_44.py
+++ problem_44.py
@@ -2,9 +2,18 @@
 # Find and fix the error
 
 def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
     fib = [0, 1]
     for i in range(2, n):
         fib.append(fib[i-1] + fib[i-2])
     return fib
+
+
 
 print(f"First 10 Fibonacci numbers: {fibonacci(10)}")
